6ed1f1ed8a5cbc7532f5281e08c74e14c2945cc1
Petro Lesapiti <petrolesapiti@gmail.com>
2024-12-04 07:53:42 +0300

    Log Levels strings learning exercise

---
+++ LogLevels.java
@@ -0,0 +1,30 @@
+public class LogLevels {
+    public static void main(String [] args){
+        LogLevels message= new LogLevels();
+        message.details();
+    }
+    public static String message(String longLine){
+        //Create an array separed by : 
+         String [] messageParts=longLine.split(":");
+         //Take the last part or the second part and trim to remove white spaces. 
+         String message=messageParts[1].trim();
+         return message;
+    }
+    public static String logLevel(String message){
+        //Split and get the first part alone. 
+        String log=message.split(":")[0];
+        //Replace these "[]" with ""
+        String logText= log.replace("]","").replace("[","");
+        //Trim to remove wgite splaces and change to lowercase. 
+        return logText.trim().toLowerCase(); 
+    }
+    public static String reformat(String message){
+        //This method utilizes the two methods above. 
+        String logLineMessage= message(message)+" ("+logLevel(message)+")";
+        return logLineMessage;
+    }
+    public void details(){
+        System.out.println(reformat("[INFO]: Operation completed"));
+    }
+    
+}
